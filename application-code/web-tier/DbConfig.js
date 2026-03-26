41c9978083b0d496730d73489ae9351497cdd140
Saahiti-Korlam <46818707+Saahiti-Korlam@users.noreply.github.com>
2026-03-26 16:12:26 +0530

    Delete application-code/web-tier/DbConfig.js

--- application-code/web-tier/DbConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    DB_HOST : 'three-tier-db.cbiwg06msjh5.ap-south-1.rds.amazonaws.com',
-    DB_USER : 'dbadmin',
-    DB_PWD : 'dbuser1234',
-    DB_DATABASE : 'webappdb'
-});
