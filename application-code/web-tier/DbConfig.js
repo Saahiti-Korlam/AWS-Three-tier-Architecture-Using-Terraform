efac1239d84532d7c7a92004a7eb6bf7b943cb9e
Saahiti-Korlam <46818707+Saahiti-Korlam@users.noreply.github.com>
2026-03-26 16:00:19 +0530

    Add DbConfig.js for database configuration
    This is database (RDS ) configuration

---
+++ application-code/web-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'three-tier-db.cbiwg06msjh5.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'dbadmin',
+    DB_PWD : 'dbuser1234',
+    DB_DATABASE : 'webappdb'
+});
